73e32fe4cd713f9911855bc8f5a14926eaa8bd48
gimi <1056619204@qq.com>
2021-04-09 22:31:13 +0800

    update

--- src/main/java/com/work/wechat/WechatAuthApplication.java
+++ src/main/java/com/work/wechat/WechatAuthApplication.java
@@ -1,14 +1,17 @@
 package com.work.wechat;
 
+import com.work.wechat.util.IPUtils;
 import com.work.wechat.util.WechatSHA1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -23,40 +26,26 @@ public class WechatAuthApplication {
 
     @GetMapping("/")
     public String index(HttpServletRequest request) {
-        String ip = this.getIP(request);
+        String ip = IPUtils.getIpAddr(request);
         log.info("当前访问ip {} ", ip);
-        return "/" + ip;
+        return ip;
     }
 
     @GetMapping("/checkToken")
-    public String checkToken(HttpServletRequest request) {
+    public boolean checkToken(HttpServletRequest request) {
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
         String token = TOKEN;
-        if (StringUtils.isEmpty(signature) || StringUtils.isEmpty(timestamp) || StringUtils.isEmpty(nonce)  ){
-            return "fail";
+        log.info("signature:{} timestamp: {} nonce:{} token:{}", signature, timestamp, nonce, token);
+        if (StringUtils.isEmpty(signature) || StringUtils.isEmpty(timestamp) || StringUtils.isEmpty(nonce)) {
+            return false;
         }
         String sign = WechatSHA1.getSHA1(token, timestamp, nonce);
-        return sign == signature ? token : "fail";
+        log.info("加密后的签名为{}", sign);
+        return Objects.equals(sign, signature) ;
 
     }
 
-    public String getIP(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("X-Real-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
 
 }
